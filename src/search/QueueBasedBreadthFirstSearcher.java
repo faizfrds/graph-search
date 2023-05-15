e31dd1696a3f879cf62bb7cfec2fd7b0e3e92f83
faiz <faiz.firdaus.2004@gmail.com>
2023-05-15 18:51:43 -0400

    iii

--- src/search/QueueBasedBreadthFirstSearcher.java
+++ src/search/QueueBasedBreadthFirstSearcher.java
@@ -28,50 +28,73 @@ public class QueueBasedBreadthFirstSearcher<T> extends Searcher<T> {
 			return solution;
 		}
 
-		final List<T> path = new ArrayList<T>();
-		Stack<T> stack = new Stack<T>();
+		List<T> predecessors = new ArrayList<T>();
+		List<T> states = new ArrayList<T>();
+		Queue<T> queue = new LinkedList<T>();
 		List<T> visitedStates = new ArrayList<T>();
+		final List<T> path = new ArrayList<T>();
 
-		if (searchProblem.isGoalState(searchProblem.getInitialState())) return null;
+		T initial = searchProblem.getInitialState();
 
-		stack.push(searchProblem.getInitialState());
+		queue.add(initial);
+		states.add(initial);
+		predecessors.add(initial);
+
+		T goal = null;
 		
-		while (!stack.empty()){
+		while (!queue.isEmpty()){
 
-			T currentState = stack.pop();
-			path.add(currentState);
+			T currentState = queue.remove();
 
 			if (searchProblem.isGoalState(currentState)){
-
-				if (isValid(path)){
-					solution = path;
-					return path;
-				} 
-				else{
-					throw new RuntimeException("searcher should never find an invalid solution!");
-				}
+				goal = currentState;
+				break;
 			}
+
 			visitedStates.add(currentState);
 
-			for (T neighbor : searchProblem.getSuccessors(currentState)){
-
-				if (!visitedStates.contains(neighbor)){
-					stack.push(neighbor);
-				}
-			}
+			for (T neighbor : searchProblem.getSuccessors(currentState)) {
+				if (!visitedStates.contains(neighbor)) {
 	
-			while (!stack.empty() && visitedStates.contains(stack.peek())){
-
-				stack.pop();
-				path.remove(path.size() - 1);
+					// if this neighbor hasn't been seen before
+					if (!states.contains(neighbor)) {
+						// add it to the list of states
+						states.add(neighbor);
+						// and set its predecessor to itself
+						predecessors.add(neighbor);
+						
+					}
+	
+					// now set the neighbor's predecessor correctly
+					predecessors.set(states.indexOf(neighbor), currentState);
+					queue.add(neighbor);
+					System.out.println(neighbor);
+				}
 			}
 
 		}
-		
-		if(!isValid(path)) throw new RuntimeException();
 
-		if (solution.isEmpty()) return null;
+		// if a goal was found
+		if (goal != null) {
+			// build a path by looking up each predecessor, starting from
+			// the goal state
+			path.add(goal);
+			while (!goal.equals(searchProblem.getInitialState())) {
+				final T predecessor = predecessors.get(states.indexOf(goal));
+				path.add(predecessor);
+				goal = predecessor;
+			}
 
+			// the path is in reverse order (goal-to-initial), so we reverse
+			// it into the correct order
+			Collections.reverse(path);
+		}
+		if (path.size() > 0) {
+			if (!isValid(path)) {
+				throw new RuntimeException(
+						"searcher should never find an invalid solution!");
+			}
+		}
 		return path;
 	}
 }
